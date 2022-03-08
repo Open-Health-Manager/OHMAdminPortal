ad7eaf14b4ef2f754a277c2e7cc5097d7009b01f
Brandon Xavier Franco <bfranco@mitre.org>
2022-03-08 13:06:57 -0600

    update to display Patien Data Receipts List on account page

--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,15 @@
-const express = require('express'); 
+const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const https = require('https');
 
 //provide a view of all resources related to a specific patient
-router.post('/retrieve_all_resources', function(req, res){
+router.post('/retrieve_all_resources', function (req, res) {
 
     const { patientID } = req.body;
     console.log("req", req.body);
 
-    const agent = new https.Agent({  
+    const agent = new https.Agent({
         rejectUnauthorized: false
     });
 
@@ -22,10 +22,62 @@ router.post('/retrieve_all_resources', function(req, res){
         console.log(data)
         res.status(200).json(response.data);
     })
-    .catch((err) => {
-        console.log(err.message)
-        res.status(500).json({ message: err.message });
-    });
+        .catch((err) => {
+            console.log(err.message)
+            res.status(500).json({ message: err.message });
+        });
 });
+
+//provide a patient data receipt related to a specific patient
+router.post('/retrieve_patient_data_receipts', function (req, res) {
+
+    const { patientID } = req.body;
+    console.log("req", req.body);
+
+    const agent = new https.Agent({
+        rejectUnauthorized: false
+    });
+
+    axios({
+        method: "GET",
+        url: `http://ohm.healthmanager.pub.aws.mitre.org:8080/fhir/MessageHeader?focus=Patient/${patientID}`,
+        httpsAgent: agent,
+    }).then(response => {
+        var data = response.data;
+        console.log(data)
+        res.status(200).json(response.data);
+    })
+        .catch((err) => {
+            console.log(err.message)
+            res.status(500).json({ message: err.message });
+        });
+});
+
+
+//provide a view of patient resource via a username
+router.post('/search_username', function (req, res) {
+
+    const { userName } = req.body;
+    console.log("req", req.body);
+
+    const agent = new https.Agent({
+        rejectUnauthorized: false
+    });
+
+    axios({
+        method: "GET",
+        url: `http://ohm.healthmanager.pub.aws.mitre.org:8080/fhir/Patient?identifier=urn%3Amitre%3Ahealthmanager%3Aaccount%3Ausername%7C${userName}`,
+        httpsAgent: agent,
+    }).then(response => {
+        var data = response.data;
+        console.log(data)
+        res.status(200).json(response.data);
+    })
+        .catch((err) => {
+            console.log(err.message)
+            res.status(500).json({ message: err.message });
+        });
+});
+
 
 module.exports = router;
--- src/components/pages/userDetails/UserDetails.js
+++ src/components/pages/userDetails/UserDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Table, Container, Row, Col, Button } from "react-bootstrap";
 import { useForm } from 'react-hook-form';
 import axios from "axios";
 
@@ -14,39 +14,100 @@ function UserDetails() {
     });
 
     const [resourcesList, setResourcesList] = useState([]);
+    const [patientDataReceiptsList, setPatientDataReceiptsList] = useState([]);
+
 
 
     const onSubmit = async (data) => {
         console.log(data)
         const response = await axios({
             method: "POST",
-            url: "http://localhost:4003/retrieve_all_resources",
+            url: "http://localhost:4003/search_username",
             data: data
         });
         var data = response.data;
-        console.log(data)
-        setResourcesList(data)
+        console.log(data.entry[0].resource.id)
+        await patient_data_receipt_list(data.entry[0].resource.id)
+        await resource_list(data.entry[0].resource.id)
     }
 
+    const resource_list = async (patientID) => {
+        console.log(patientID)
+        const response = await axios({
+            method: "POST",
+            url: "http://localhost:4003/retrieve_all_resources",
+            data: {
+                patientID: patientID
+            },
+        })
+        var data = response.data;
+        console.log(data)
+        setResourcesList(data)
+        console.log("resource list retrieval succesful");
+    }
+
+    const patient_data_receipt_list = async (patientID) => {
+        console.log(patientID)
+        const response = await axios({
+            method: "POST",
+            url: "http://localhost:4003/retrieve_patient_data_receipts",
+            data: {
+                patientID: patientID
+            },
+        })
+        var data = response.data;
+        console.log(data)
+        setPatientDataReceiptsList(data)
+        console.log("patient data receipt list retrieval succesful");
+    }
 
     return (
         <Container fluid className="content-block">
             <Row style={{ paddingTop: "20px" }}>
                 <Col md={6}>
-                    <h1>User Deatils ID Search</h1>
+                    <h1>User Details Search</h1>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <input type="text" className="form-control" {...register("patientID", { required: true })} />
-                        {errors.patientID && <p className="error-text">patientID is required</p>}
+                        <input type="text" className="form-control" {...register("userName", { required: true })} />
+                        {errors.userName && <p className="error-text">user name is required</p>}
                         <Button variant='form' type="submit">Submit</Button>
                     </form>
                 </Col>
             </Row>
+            {patientDataReceiptsList?.entry &&
+                <Row>
+                    <Col md={6}>
+                        <h2 style={{ paddingTop: "30px" }}>Patient Data Receipts List</h2>
+
+                        <Table striped bordered hover variant="dark" responsive="lg">
+                            <thead>
+                                <tr>
+                                    <th>Type</th>
+                                    <th>Last Updated</th>
+                                    <th>Bundle URL</th>
+                                    <th>Resource Content URL</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {patientDataReceiptsList.entry.map((entry, index) => (
+                                    <tr className="tableList" key={index}>
+                                        <td>{entry.resource.resourceType}</td>
+                                        <td>{new Date(entry.resource.meta.lastUpdated).toLocaleString()}</td>
+                                        <td><a href={"http://ohm.healthmanager.pub.aws.mitre.org:8080/fhir/" + entry.resource.focus[1].reference}>{"http://ohm.healthmanager.pub.aws.mitre.org:8080/fhir/" + entry.resource.focus[1].reference}</a></td>
+                                        <td><a href={entry.fullUrl}>{entry.fullUrl}</a></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+
+                    </Col>
+                </Row>
+            }
             {resourcesList?.entry &&
                 <Row>
                     <Col md={6}>
                         <h2 style={{ paddingTop: "30px" }}>Resources List</h2>
 
-                        <table>
+                        <Table striped bordered hover variant="dark" responsive="lg">
                             <thead>
                                 <tr>
                                     <th>Type</th>
@@ -54,14 +115,16 @@ function UserDetails() {
                                     <th>Resource Content URL</th>
                                 </tr>
                             </thead>
-                            {resourcesList.entry.map((entry, index) => (
-                                <tr className="resourceList" key={index}>
-                                    <td>{entry.resource.resourceType}</td>
-                                    <td>{new Date(entry.resource.meta.lastUpdated).toLocaleString()}</td>
-                                    <td><a href={"http://ohm.healthmanager.pub.aws.mitre.org:8080/fhir/" + entry.resource.resourceType + "/" + entry.resource.id}>{"http://ohm.healthmanager.pub.aws.mitre.org:8080/fhir/" + entry.resource.resourceType + "/" + entry.resource.id}</a></td>
-                                </tr>
-                            ))}
-                        </table>
+                            <tbody>
+                                {resourcesList.entry.map((entry, index) => (
+                                    <tr className="tableList" key={index}>
+                                        <td>{entry.resource.resourceType}</td>
+                                        <td>{new Date(entry.resource.meta.lastUpdated).toLocaleString()}</td>
+                                        <td><a href={entry.fullUrl}>{entry.fullUrl}</a></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
 
                     </Col>
                 </Row>
